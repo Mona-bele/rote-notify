2e130986652df46b0de85ba5a81e087d81cb8f6e
Paulo Lopes Estevão <pl1745240@gmail.com>
2024-12-10 13:02:05 +0000

    fix: update LoadEnv to accept a path parameter and simplify getEnv function

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -16,30 +16,25 @@ type Env struct {
 }
 
 func LoadEnv(path string) *Env {
-	_ = godotenv.Load()
+	_ = godotenv.Load(path)
 
 	return &Env{
-		RabbitmqUrl:         getEnv("RABBITMQ_URL", "RABBITMQ_URL_LOCALHOST"),
-		JwtNotifyPrivateKey: getEnv("JWT_NOTIFY_PRIVATE_KEY", ""),
-		JwtKid:              getEnv("JWT_KID", ""),
-		JwtIssuer:           getEnv("JWT_ISSUER", ""),
-		JwtSubject:          getEnv("JWT_SUBJECT", ""),
-		JwtAudience:         getEnv("JWT_AUDIENCE", ""),
+		RabbitmqUrl:         getEnv("RABBITMQ_URL"),
+		JwtNotifyPrivateKey: getEnv("JWT_NOTIFY_PRIVATE_KEY"),
+		JwtKid:              getEnv("JWT_KID"),
+		JwtIssuer:           getEnv("JWT_ISSUER"),
+		JwtSubject:          getEnv("JWT_SUBJECT"),
+		JwtAudience:         getEnv("JWT_AUDIENCE"),
 	}
 
 }
 
-func getEnv(key, defaultKey string) string {
-
-	if defaultKey != "" {
-		key = defaultKey
-	}
+func getEnv(key string) string {
 
 	val := os.Getenv(key)
 	if val == "" {
 		log.Error().Msgf("Environment variable %s is not set", key)
 		panic("Environment variable not set")
-		return ""
 	}
 	return val
 }
